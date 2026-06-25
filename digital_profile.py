24b99e856efef45b3baf9bc61ec0b87bfcd201ff
413-06-sasinutda <54244@yupparaj.ac.th>
2026-06-25 09:47:20 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+st.title("My Digital Profile")
+
+
+st.header("แนะนำตัว")
+st.write("นางสาวศศินัดดา วงษา")
+st.write("ชื่อเล่น มีนา")
+st.write("เลขที่ 6 ม.4/13")
+
+
+st.header("เป้าหมายและสิ่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย : **อยากเรียนด้านคอมพิวเตอร์และเทคโนโลยี")
+st.write("สิ่งที่อยากเป็นในอนาคต : นักแคสเกม (Game Caster)")
